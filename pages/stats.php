d002fd410ec54ca1b0b46fe7b3f5ec845010b51b
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-13 09:31:28 +0200

    Order stats display by timestamp

--- pages/stats.php
+++ pages/stats.php
@@ -2,7 +2,7 @@
 
 $fragment = new rex_fragment();
 
-$list = rex_list::factory('select id, timestamp, page, referer from naju_visitor_stat');
+$list = rex_list::factory('select id, timestamp, page, referer from naju_visitor_stat order by timestamp desc');
 
 $fragment->setVar('content', $list->get(), false);
 echo $fragment->parse('core/page/section.php');
