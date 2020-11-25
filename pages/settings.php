fa44373851406db8d2f1dc93a662860209ebaad9
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-11-25 21:16:41 +0100

    Add option cockpit

---
+++ pages/settings.php
@@ -0,0 +1,30 @@
+<?php
+
+$func = rex_request('func', 'string', '');
+
+$msg = '';
+
+if ($func === 'stats_clear') {
+    rex_sql::factory()->setTable('naju_stats')->delete();
+    $msg .= '<div class="alert alert-info">Statistiken wurden zurückgesetzt</div>';
+}
+
+$fragment = new rex_fragment();
+
+$content = '';
+
+$content .= '
+    <form method="post" action="' . rex_url::currentBackendPage(['func' => 'stats_clear']) . '">
+        <div class="form-group">
+            <label for="clear-stats">Statistiken zurücksetzen</label>
+            <button type="submit" id="clear-stats" class="btn btn-link">
+                <i class="rex-icon fa-cogs"></i> ausführen
+            </button>
+        </div>
+    </form>
+';
+
+$fragment->setVar('title', 'Optionen', false);
+$fragment->setVar('class', 'edit', false);
+$fragment->setVar('body', $msg . $content, false);
+echo $fragment->parse('core/page/section.php');
